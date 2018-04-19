3b25f8057938218d9968ba0ade629a780c9e47fe
wroclawianka <dorota.zelga@gmail.com>
2018-04-19 21:15:19 +0200

    second scroll gallery added

--- script.js
+++ script.js
@@ -1,23 +1,28 @@
-    //smooth scrolling
-    $('a[href*="#"]').click(function(event) {
-        var target = $(this.hash);
-        if (target.length) {
-            event.preventDefault();
-            $('html, body').animate({
-                scrollTop: target.offset().top - 50
-            }, 1000, function() {});
-        }
-    });
-    //gallery buttons - uwaga, bedziesz miec dwie takie (parenty itd)
-    $('.scrollleft').click(function() {
-        console.log('to the left')
-        $('#scrollbar').animate({
-            scrollLeft: '-=400'
-        }, 500, 'easeOutQuad');
-    });
-    $('.scrollright').click(function() {
-        console.log('to the right')
-        $('#scrollbar').animate({
-            scrollLeft: '+=400'
-        }, 500, 'easeOutQuad');
-    });
+//smooth page scrolling
+$('a[href*="#"]').click(function(event) {
+    var target = $(this.hash);
+    if (target.length) {
+        event.preventDefault();
+        $('html, body').animate({
+            scrollTop: target.offset().top - 50
+        }, 1000, function() {});
+    }
+});
+
+//gallery scrolling
+function scrollGallery(element, value) {
+    var gallery = $(element).parents(".gallery");
+    var scrollGallery = gallery.children(".scroll-gallery")
+
+    scrollGallery.animate({
+        scrollLeft: value
+    }, 500, 'easeOutQuad');
+}
+
+$('.scrollleft').click(function() {
+    scrollGallery(this, '-=400')
+});
+
+$('.scrollright').click(function() {
+    scrollGallery(this, '+=400')
+});
