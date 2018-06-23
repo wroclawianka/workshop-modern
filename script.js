ecb8e6f84a1c881cd0dec4887cefd39cc2e3b81d
wroclawianka <dorota.zelga@gmail.com>
2018-06-23 11:09:40 +0200

    gallery overlay impelemented

--- script.js
+++ script.js
@@ -1,28 +1,49 @@
-$('.scrollleft').click(function() {
-    scrollGallery(this, '-=400')
-});
+$('.scrollleft').click(function () {
+  scrollGallery(this, '-=400')
+})
 
-$('.scrollright').click(function() {
-    scrollGallery(this, '+=400')
-});
+$('.scrollright').click(function () {
+  scrollGallery(this, '+=400')
+})
 
-//smooth page scrolling
-$('a[href*="#"]').click(function(event) {
-    var target = $(this.hash);
-    if (target.length) {
-        event.preventDefault();
-        $('html, body').animate({
-            scrollTop: target.offset().top - 50
-        }, 1000, function() {});
-    }
-});
+// smooth page scrolling
+$('a[href*="#"]').click(function (event) {
+  var target = $(this.hash)
+  if (target.length) {
+    event.preventDefault()
+    $('html, body').animate({
+      scrollTop: target.offset().top - 50
+    }, 1000, function () {})
+  }
+})
 
-//gallery scrolling
-function scrollGallery(element, value) {
-    var gallery = $(element).parents(".gallery");
-    var scrollGallery = gallery.children(".scroll-gallery");
+// gallery scrolling
+function scrollGallery (element, value) {
+  var gallery = $(element).parents('.gallery')
+  var scrollGallery = gallery.children('.scroll-gallery')
 
-    scrollGallery.animate({
-        scrollLeft: value
-    }, 500, 'easeOutQuad');
+  scrollGallery.animate({
+    scrollLeft: value
+  }, 500, 'easeOutQuad')
 }
+
+// gallery
+const overlay = document.querySelector('.overlay');
+const overlayImage = overlay.querySelector('img');
+const overlayClose = overlay.querySelector('.close');
+
+function handleClick (e) {
+  const src = e.currentTarget.src
+  overlayImage.src = src
+  overlay.classList.add('open')
+}
+
+function close () {
+  overlay.classList.remove('open')
+}
+
+const items = document.querySelectorAll('.gallery_item');
+
+items.forEach(item => item.addEventListener('click', handleClick));
+
+overlayClose.addEventListener('click', close);
